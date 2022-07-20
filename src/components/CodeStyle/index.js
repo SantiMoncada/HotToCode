0362b8b0e54a319a1bd9452eaaeeca96a22aa251
Santiago Moncada <90731161+SantiMoncada@users.noreply.github.com>
2022-07-20 13:36:44 +0200

    styles for snippets

--- src/components/CodeStyle/index.js
+++ src/components/CodeStyle/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Highlight, { defaultProps } from "prism-react-renderer";
-import theme from 'prism-react-renderer/themes/synthwave84';
+import theme from 'prism-react-renderer/themes/github';
 import { Container } from "react-bootstrap";
 
-const CodeStyle = ({ className: classNameParent, code }) => {
+const CodeStyle = ({ className: classNameParent, code, language }) => {
     return (
         <div >
-            <Highlight {...defaultProps} code={code} language="jsx" theme={theme}>
+            <Highlight {...defaultProps} code={code} language={language} theme={theme}>
                 {({ className, style, tokens, getLineProps, getTokenProps }) => (
                     <pre className={`${className} ${classNameParent}`} style={style}>
                         <Container>
