5f2910cc8cc22132dee06743b23086b909e6f52b
Santiago Moncada <90731161+SantiMoncada@users.noreply.github.com>
2022-07-28 13:24:43 +0200

    syle fixes

--- src/config/socket-config.js
+++ src/config/socket-config.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
 const URL = process.env.REACT_APP_BACK_END_URL
-const socket = io(URL || 'http://192.168.169.101:5005', { autoConnect: false })
+const socket = io(URL || 'http://localhost:5005', { autoConnect: false })
 
 export default socket;
